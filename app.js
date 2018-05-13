653383bcf5824e57e94dfae02345d4c02031999f
100drine <julien100drine@sfr.fr>
2018-05-13 22:52:21 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+console.log('have fun !');
+
+$("body").css({
+    'margin': '20px',
+    'font': '14pt Arial',
+    'background-color': '#E37B40',
+    'width': '100px',
+    'heigth': '50px',
+    'color':'#46B29D',
+})
+
+$("h1").css({
+    'color':'#324D5C',
+
+});
+
+
+/*
+$(document).ready(function() {
+    
+  $("input").on ("keyup",function(){
+    
+    var firstName= $(".first_name").val();
+    console.log(firstName);
+    var lastName = $(".last_name").val();
+    console.log(lastName);
+    var city = $(".city").val();
+    console.log(city);
+    $(".username").text(firstName + " " +lastName)
+
+});          
+}); 
+*/
+
+
+
+
